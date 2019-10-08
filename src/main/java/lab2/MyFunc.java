02e1de2486395d7210ee36a94320a270887bdfe9
Artur Gogiyan <artur.gogiyan@gmail.com>
2019-10-09 00:18:05 +0300

    func implementation

--- src/main/java/lab2/MyFunc.java
+++ src/main/java/lab2/MyFunc.java
@@ -2,13 +2,28 @@ package lab2;
 
 public class MyFunc {
 
-    private IMyMath myMath;
+    private final int accuracy = 4;
 
-    public IMyMath getMyMath() {
-        return myMath;
+    public double Calculate(double x){
+        return x > 0 ? lowerEquation(x) : upperEquation(x);
     }
 
-    public void setMyMath(IMyMath myMath) {
-        this.myMath = myMath;
+    private double upperEquation(double x){
+       double result = (getMath().sec(x, accuracy) - getMath().cos(x, accuracy) + getMath().sec(x,accuracy)) / (getMath().tan(x,accuracy) + getMath().sec(x,accuracy));
+       result += getMath().sin(x,accuracy) - getMath().sec(x,accuracy);
+       result = result * result;
+       return result;
+    }
+
+    private double lowerEquation(double x){
+        double result = (getMath().log3(x,accuracy) * getMath().log3(x,accuracy)) / (getMath().log10(x,accuracy) * getMath().log5(x,accuracy));
+        result -= getMath().log5(x,accuracy) * getMath().ln(x,accuracy);
+        result /= getMath().ln(x,accuracy);
+        result += getMath().ln(x,accuracy) - (getMath().log2(x,accuracy) / getMath().log5(x,accuracy)) + getMath().log3(x,accuracy);
+        return result;
+    }
+
+    protected IMyMath getMath(){
+        return new MyMath();
     }
 }
